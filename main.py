4a83d674d48d9a340463f7d6f124f5e9e62dd96b
klimentlamonov <1_0_91@bk.ru>
2020-12-04 00:23:07 +0300

    Start new feature

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import vk_api
-import sqlite3
-from vk_api.longpoll import VkLongPoll, VkEventType
-from secret import main_token
-
-vk_session = vk_api.VkApi(token=main_token)
-session_api = vk_session.get_api()
-longpoll = VkLongPoll(vk_session)
-
-
-def send_message(id, text):
-    vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
+from logic import *
 
 
 for event in longpoll.listen():
